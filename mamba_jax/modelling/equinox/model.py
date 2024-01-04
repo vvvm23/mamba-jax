3dc2b2bea694b01e356e55cb6316330ba44495ad
Alex McKinney <alex.f.mckinney@gmail.com>
2024-01-04 11:51:16 +0000

    `MambaLLM` implementation

--- mamba_jax/modelling/equinox/model.py
+++ mamba_jax/modelling/equinox/model.py
@@ -89,6 +89,7 @@ class MambaModel(eqx.Module):
         key: jax.random.PRNGKey = None,
     ):
         super().__init__()
+        # TODO: auto-pad vocab to mult of 8
 
         key, subkey = jax.random.split(key)
         self.embedding = nn.Embedding(vocab_size, dim, key=subkey)
@@ -138,21 +139,74 @@ class MambaModel(eqx.Module):
 # https://github.com/state-spaces/mamba/blob/main/mamba_ssm/models/mixer_seq_simple.py
 # essentially, the `MambaLMHeadModel` class
 class MambaLLM(eqx.Module):
-    def __init__(self):
-        pass
+    model: MambaModel
+    lm_head: nn.Linear
 
-    def __call__(self, x: jax.Array) -> jax.Array:
-        pass
+    def __init__(
+        self,
+        dim: int,
+        num_layers: int,
+        vocab_size: int,
+        state_dim: int = 16,
+        kernel_size: int = 4,
+        expand: int = 2,
+        dt_rank: Literal["auto"] | int = "auto",
+        dt_min: float = 0.001,
+        dt_max: float = 0.1,
+        dt_init: Literal["constant", "random"] = "random",
+        dt_scale: float = 1.0,
+        dt_init_floor: float = 1e-4,
+        conv_bias: bool = True,
+        bias: bool = False,
+        use_kernel: bool = False,
+        norm_eps: float = 1e-5,
+        # TODO: add norm type (rms or layer)
+        res_dtype: jnp.dtype = jnp.float32,
+        dtype: jnp.dtype = jnp.float32,
+        key: jax.random.PRNGKey = None,
+    ):
+        super().__init__()
+
+        model_key, head_key = jax.random.split(key)
+
+        self.model = MambaModel(
+            dim=dim,
+            num_layers=num_layers,
+            vocab_size=vocab_size,
+            state_dim=state_dim,
+            kernel_size=kernel_size,
+            expand=expand,
+            dt_rank=dt_rank,
+            dt_min=dt_min,
+            dt_max=dt_max,
+            dt_init=dt_init,
+            dt_scale=dt_scale,
+            dt_init_floor=dt_init_floor,
+            conv_bias=conv_bias,
+            bias=bias,
+            use_kernel=use_kernel,
+            norm_eps=norm_eps,
+            # TODO: add norm type (rms or layer)
+            res_dtype=res_dtype,
+            dtype=dtype,
+            key=model_key,
+        )
+
+        self.lm_head = nn.Linear(dim, vocab_size, use_bias=False, key=head_key)
+
+    def __call__(self, input_ids: jax.Array) -> jax.Array:
+        x = self.model(input_ids)
+        return jax.vmap(self.lm_head)(x)
 
 
 if __name__ == "__main__":
-    L, N = 4096, 128
+    L, N = 16384, 128
     num_layers = 12
     vocab_size = 10_000
     key = jax.random.PRNGKey(0)
 
     x = jax.random.randint(key, (L,), minval=0, maxval=vocab_size)
-    model = MambaModel(N, num_layers, vocab_size, key=key)
+    model = MambaLLM(N, num_layers, vocab_size, key=key)
 
     y = model(x)
     print(y)
